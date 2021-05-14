8134a065452704c006dc1804bdc5658d62bb6d3f
alex <alex_goca50@hotmail.com>
2021-05-13 18:13:50 -0600

    second commit

---
+++ src/main/java/com/platzi/market/domain/Product.java
@@ -0,0 +1,63 @@
+package com.platzi.market.domain;
+
+public class Product {
+    private int productId;
+    private String name;
+    private int category;
+    private double price;
+    private int stock;
+    private boolean active;
+    private Category category;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// this class service functions like an intermediate class
+// between the controller and repository
+// this also could be "Component"
+// Service or Component Layer!!
+@Service
+public class ProductService {
+    // we can use autowired, because "ProductoRepository"
+    // has the notation @Repository
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
